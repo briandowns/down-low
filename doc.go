db09dfcadcc6e1483636ae2a0e47e595bd594a98
bdowns <brian.downs@gmail.com>
2014-11-23 14:08:16 -0700

    tiny doc update

--- doc.go
+++ doc.go
@@ -1,4 +1,5 @@
 // down-low is an application that allows for the transmission of secure data
 // over a number of mediums which include: Twitter, Gmail, and a number of
 // others to follow.
+// -key="/path/to/key" -service="service" -to="user@service" -m
 package main
